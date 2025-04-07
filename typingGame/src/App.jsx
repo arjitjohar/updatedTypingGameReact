b4b75d30e1437e60c6848397ec7dc0b4e440fe1a
arjitjohar <arjitjohar@gmail.com>
2025-04-07 11:41:22 -0400

    add test styling

--- typingGame/src/App.jsx
+++ typingGame/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       
         <TypingGame />
       
-        <h1 class="text-3xl font-bold underline">
+        <h1 class="text-3xl font-bold underline bg-red-500">
       Hello world!
 </h1>  
     </>

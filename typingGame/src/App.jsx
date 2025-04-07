ee06e7ddc56dd07da37fe541545b2874cce051c5
arjitjohar <arjitjohar@gmail.com>
2025-04-07 11:34:55 -0400

    fix tailwind implementation issue

--- typingGame/src/App.jsx
+++ typingGame/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import TypingGame from './components/TypingGame'
 
@@ -9,7 +7,12 @@ function App() {
 
   return (
     <>
-      <TypingGame />
+      
+        <TypingGame />
+      
+        <h1 class="text-3xl font-bold underline">
+      Hello world!
+</h1>  
     </>
   )
 }

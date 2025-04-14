d0088b8f2ecc5950f9889b623d6f545bc7a535d1
arjitjohar <arjitjohar@gmail.com>
2025-04-14 16:25:12 -0400

    front end init correctly

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import CenteredText from './components/CenteredText'
+
+function App() {
+
+  var placeholder = "this is a sample paragraph."
+
+  return (
+    <>
+      <div className=''>
+        <CenteredText/>
+      </div>
+    </>
+  )
+}
+
+export default App

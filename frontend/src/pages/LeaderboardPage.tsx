a6125567f29037a714cf7480c1dad1119a93e001
arjitjohar <arjitjohar@gmail.com>
2025-04-16 14:36:43 -0400

    add under contruction update to learderboard page

--- frontend/src/pages/LeaderboardPage.tsx
+++ frontend/src/pages/LeaderboardPage.tsx
@@ -9,7 +9,9 @@ const LeaderboardPage = () => {
     return (
         <div className="min-h-screen bg-gray-900 text-gray-300 flex flex-col items-center justify-center pb-48 font-sans">
             <p className='text-5xl font-bold'> Leaderboard </p>
+            <p className='bg-yellow-200 text-black border-2 rounded-2xl px-2 py-2 font-bold mt-2'> under construction </p>
             <div>
+                
                 <ul className='list-decimal pt-8 pr-32 font-bold font-sans'>
                     <li>97 wpm</li>
                     <li>94 wpm</li>
